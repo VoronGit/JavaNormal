8e837b55ddeb2071fec66bb07c428f2cea8a8af0
VoronGit <evilvickers@gmail.com>
2022-05-12 21:04:59 +0300

    PaintMeGrass class added.

--- src/Stepik/lesson_3_3/MainFrame.java
+++ src/Stepik/lesson_3_3/MainFrame.java
@@ -1,4 +1,15 @@
 package Stepik.lesson_3_3;
 
-public class MainFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame{
+
+    public MainFrame (String title, int x, int y, Component component) {
+        setDefaultLookAndFeelDecorated(true);
+        setTitle(title);
+        setVisible(true);
+        setBounds(400,200,x,y);
+        add(component);
+    }
 }
---
+++ src/Stepik/lesson_3_3/PaintMeGrass.java
@@ -0,0 +1,41 @@
+package Stepik.lesson_3_3;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static java.io.File.*;
+
+public class PaintMeGrass {
+    public static void main(String[] args) {
+        new MainFrame("PaintMeGrass", 800, 800, new Grass());
+    }
+}
+
+class Grass extends JPanel{
+    BufferedImage grass;
+    {
+        try {
+            grass = ImageIO.read(new File("."+ separator+"src"+separator+"Stepik"+separator+"lesson_3_3"+separator+"img"+separator+"Grass.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected void paintComponent(Graphics g) {
+        int curX = 0;
+        int curY = 0;
+        Graphics2D g2d = (Graphics2D) g;
+        while (curY < getHeight()) {
+            while (curX < getWidth()) {
+                g2d.drawImage(grass,curX,curY,this);
+                curX += grass.getWidth();
+            }
+            curY += grass.getHeight();
+            curX =0;
+        }
+    }
+}
