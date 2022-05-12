d3612a89c45a2bd3f36374893130865ccdf35eb5
VoronGit <evilvickers@gmail.com>
2022-05-12 09:25:28 +0300

    Race class is added.

--- src/Stepik/Race.java
+++ src/Stepik/Race.java
@@ -13,19 +13,25 @@ public class Race {
     public static void main(String[] args) {
         cars.add(new Cars("Ferrari"));
         cars.add(new Cars("Passat"));
-        cars.add(new Cars("Passat"));
         cars.add(new Cars("Camry"));
-        MainFrame.createMainFrame(new MakeThemRace(), 1000, Cars.fullCarsHeight + 60, "MovingImage");
+        cars.add(new Cars("Ferrari"));
+        cars.add(new Cars("Camry"));
+        cars.add(new Cars("Passat"));
+        MainFrame.createMainFrame(new MakeThemRace(), 1000, Cars.getMaxHeight() + 100, "MovingImage");
     }
 }
 class MakeThemRace extends JPanel {
     Timer timer = null;
+    static public String winner = "";
+    Color color = new Color(0,0,0,0);
     public MakeThemRace() {
-        timer  = new Timer(40, e -> {
+        timer  = new Timer(60, e -> {
             for (Cars car : Race.cars) {
                 car.x += (int) (Math.random() * 5) + 1;
-                if (car.x + car.img.getWidth() >= 1000) {
+                if (car.x + car.img.getWidth() >= getWidth() && car.x > 10) {
                     timer.stop();
+                    winner = car.name;
+                    color = new Color(0,0,0);
                 }
             }
             repaint();
@@ -39,18 +45,20 @@ class MakeThemRace extends JPanel {
         Graphics2D g2d = (Graphics2D) g.create();
         setBackground(Color.white);
         for (Cars car : Race.cars) {
-        g2d.drawImage(car.img, car.x, getHeight()/2 - Cars.fullCarsHeight/2 + car.carMax * (car.line - 1) + car.carMax/2 - car.img.getHeight()/2, this);
+        g2d.drawImage(car.img, car.x, getHeight()/2 - Cars.getMaxHeight()/2 + car.getCarHeight() + car.getCarPos(), this);
         }
+        g2d.setColor(color);
+        g2d.setFont(new Font(null,Font.BOLD,40));
+        g2d.drawString(MakeThemRace.winner + " is a winner!", getWidth()/2 - 150, getHeight()/2);
         g2d.dispose();
     }
 }
 
 class Cars {
-    public int carMax = 70;
+    static public int carMax = 0;
     protected BufferedImage img;
     protected int line;
     protected String name;
-    public static int fullCarsHeight = 0;
     protected int x;
     static private int nextLine = 1;
     public Cars (String img){
@@ -63,8 +71,8 @@ class Cars {
                 throw new RuntimeException(ex);
             }
         }
+        if (carMax < this.img.getHeight() + 10) carMax = this.img.getHeight() + 10;
         this.x = 0;
-        this.fullCarsHeight += carMax;
         this.name = img;
         this.line = nextLine;
         nextLine++;
@@ -72,5 +80,14 @@ class Cars {
 
     public static int getNextLine() {
         return nextLine;
+    }
+    public static int getMaxHeight() {
+        return carMax*(nextLine-1);
+    }
+    public int getCarHeight() {
+        return carMax*(line-1);
+    }
+    public int getCarPos() {
+        return carMax/2 - this.img.getHeight()/2;
     }
 }
