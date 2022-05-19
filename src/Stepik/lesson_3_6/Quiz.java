5ca697da6f0535c2ada746bbcd0d4f0dc3e1b0e4
VoronGit <evilvickers@gmail.com>
2022-05-19 09:18:05 +0300

    Quiz class updated.

--- src/Stepik/lesson_3_6/Quiz.java
+++ src/Stepik/lesson_3_6/Quiz.java
@@ -11,42 +11,63 @@ public class Quiz {
     private final static ArrayList<Question> qSet = new ArrayList<>();
     private static int score;
     private static int num = 0;
+
     public static void main(String[] args) {
-        qSet.add(new Question("Из какого фильма Прекрасный принц?", new String[] {"Золушка","Русалочка","Спящая красавица","Мулан"}, (byte) 1));
-        qSet.add(new Question("Из какого фильма Прекрасный принц?", new String[] {"Золушка","Русалочка","Спящая красавица","Мулан"}, (byte) 1));
-        qSet.add(new Question("Из какого фильма Прекрасный принц?", new String[] {"Золушка","Русалочка","Спящая красавица","Мулан"}, (byte) 1));
-        qSet.add(new Question("Из какого фильма Прекрасный принц?", new String[] {"Золушка","Русалочка","Спящая красавица","Мулан"}, (byte) 1));
-        qSet.add(new Question("Из какого фильма Прекрасный принц?", new String[] {"Золушка","Русалочка","Спящая красавица","Мулан"}, (byte) 1));
-        new MainFrame("Quiz", 800,400,new QuizGUI());
+        qSet.add(new Question("Из какого фильма Прекрасный принц?", new String[]{"Золушка", "Русалочка", "Спящая красавица", "Мулан"}, (byte) 1));
+        qSet.add(new Question("Из какого фильма Прекрасный принц?", new String[]{"Золушка", "Русалочка", "Спящая красавица", "Мулан"}, (byte) 1));
+        qSet.add(new Question("Из какого фильма Прекрасный принц?", new String[]{"Золушка", "Русалочка", "Спящая красавица", "Мулан"}, (byte) 1));
+        qSet.add(new Question("Из какого фильма Прекрасный принц?", new String[]{"Золушка", "Русалочка", "Спящая красавица", "Мулан"}, (byte) 1));
+        qSet.add(new Question("Из какого фильма Прекрасный принц?", new String[]{"Золушка", "Русалочка", "Спящая красавица", "Мулан"}, (byte) 1));
+        new MainFrame("Quiz", 800, 400, new QuizGUI());
     }
-    public static void addScore(){ score++; }
-    public static int getScore(){ return score; }
-    public static Question getQuestion(){ return qSet.get(num); }
-    public static void addNum() { num++; }
-    public static int getNum() { return num + 1; }
-    public static int getQAmount(){ return qSet.size(); }
+
+    public static void addScore() {
+        score++;
+    }
+
+    public static int getScore() {
+        return score;
+    }
+
+    public static Question getQuestion() {
+        return qSet.get(num);
+    }
+
+    public static void addNum() {
+        num++;
+    }
+
+    public static int getNum() {
+        return num + 1;
+    }
+
+    public static int getQAmount() {
+        return qSet.size();
+    }
 }
-class QuizGUI extends JPanel{
+
+class QuizGUI extends JPanel {
     static JLabel counter = new JLabel();
     static JLabel question = new JLabel();
+    static JPanel topPanel = new JPanel();
+    static JPanel questionPanel = new JPanel();
     static JPanel bottomPanel = new JPanel();
     static ArrayList<Button> buttons = new ArrayList<>();
-    public QuizGUI(){
+
+    public QuizGUI() {
         setBackground(Color.WHITE);
-        JPanel topPanel = new JPanel();
-        BoxLayout topPanelLayout = new BoxLayout(topPanel,BoxLayout.LINE_AXIS);
+        BoxLayout topPanelLayout = new BoxLayout(topPanel, BoxLayout.LINE_AXIS);
         topPanel.setLayout(topPanelLayout);
 
-        counter.setFont(new Font(null,Font.BOLD,16));
+        counter.setFont(new Font(null, Font.BOLD, 16));
         counter.setText("Вопрос 1 из " + Quiz.getQAmount());
         counter.setFocusable(false);
         topPanel.add(Box.createHorizontalGlue());
         topPanel.setBackground(Color.WHITE);
         topPanel.add(counter);
-        topPanel.add(Box.createRigidArea(new Dimension(20,0)));
+        topPanel.add(Box.createRigidArea(new Dimension(20, 0)));
 
-        JPanel questionPanel = new JPanel();
-        GridLayout questionPanelLayout = new GridLayout(1,1);
+        GridLayout questionPanelLayout = new GridLayout(1, 1);
         questionPanel.setLayout(questionPanelLayout);
         questionPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 20));
         questionPanel.setBackground(Color.WHITE);
@@ -54,33 +75,33 @@ class QuizGUI extends JPanel{
         question.setText(Quiz.getQuestion().question);
         question.setFocusable(false);
         question.setFont(new Font(null, Font.BOLD, 16));
-        question.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
+        question.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
         question.setHorizontalAlignment(SwingConstants.CENTER);
         question.setVerticalAlignment(SwingConstants.CENTER);
 
         question.setBackground(Color.WHITE);
         questionPanel.add(question);
 
-        GridLayout buttonGrid = new GridLayout(2,2);
+        GridLayout buttonGrid = new GridLayout(2, 2);
         bottomPanel.setLayout(buttonGrid);
         bottomPanel.setBackground(Color.WHITE);
 
         getButtons();
 
-        BoxLayout mainLayout = new BoxLayout(this,BoxLayout.PAGE_AXIS);
+        BoxLayout mainLayout = new BoxLayout(this, BoxLayout.PAGE_AXIS);
         setLayout(mainLayout);
-        add(Box.createRigidArea(new Dimension(0,10)));
+        add(Box.createRigidArea(new Dimension(0, 10)));
         add(topPanel);
-        add(Box.createRigidArea(new Dimension(0,5)));
+        add(Box.createRigidArea(new Dimension(0, 5)));
         add(questionPanel);
         add(bottomPanel);
-        add(Box.createRigidArea(new Dimension(0,5)));
+        add(Box.createRigidArea(new Dimension(0, 5)));
     }
 
-    public static void getButtons(){
+    public static void getButtons() {
         for (String ans : Quiz.getQuestion().giveAnswersInRandom()) {
             Button b = new Button(ans);
-            b.setFont(new Font(null, Font.BOLD,16));
+            b.setFont(new Font(null, Font.BOLD, 16));
             b.setActionCommand(ans);
             b.addActionListener(new ButtonActHandlerQuiz());
             b.setBackground(Color.LIGHT_GRAY);
@@ -109,10 +130,11 @@ class Question {
         this.answers = answers;
         this.correctAnswer = correctAnswer;
     }
-    public String[] giveAnswersInRandom(){
+
+    public String[] giveAnswersInRandom() {
         HashSet<Integer> usedPositions = new HashSet<>();
         String[] answersFinal = new String[answers.length];
-        for (int i = 0; i < answers.length;) {
+        for (int i = 0; i < answers.length; ) {
             int r = (int) (Math.random() * 4);
             if (!usedPositions.contains(r)) {
                 answersFinal[r] = answers[i];
@@ -122,23 +144,31 @@ class Question {
         }
         return answersFinal;
     }
+
     public boolean checkAnswer(String attempt) {
         return attempt.equals(answers[correctAnswer - 1]);
     }
 }
+
 class ButtonActHandlerQuiz implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (Quiz.getQuestion().checkAnswer(e.getActionCommand())) Quiz.addScore();
         System.out.println(Quiz.getScore());
-        Quiz.addNum();
-        QuizGUI.counter.setText("Вопрос " + Quiz.getNum() + " из " + Quiz.getQAmount());
-        QuizGUI.question.setText(Quiz.getQuestion().question);
-        String[] ans = Quiz.getQuestion().giveAnswersInRandom();
-        for (int i = 0; i < 4; i++) {
-            Button b = (Button) QuizGUI.buttons.toArray()[i];
-            b.setLabel(ans[i]);
-            b.setActionCommand(ans[i]);
+        if (Quiz.getNum() < Quiz.getQAmount()) {
+            Quiz.addNum();
+            QuizGUI.counter.setText("Вопрос " + Quiz.getNum() + " из " + Quiz.getQAmount());
+            QuizGUI.question.setText(Quiz.getQuestion().question);
+            String[] ans = Quiz.getQuestion().giveAnswersInRandom();
+            for (int i = 0; i < 4; i++) {
+                Button b = (Button) QuizGUI.buttons.toArray()[i];
+                b.setLabel(ans[i]);
+                b.setActionCommand(ans[i]);
+            }
+        } else {
+            QuizGUI.topPanel.setVisible(false);
+            QuizGUI.questionPanel.setVisible(false);
+            QuizGUI.bottomPanel.setVisible(false);
         }
     }
 }
