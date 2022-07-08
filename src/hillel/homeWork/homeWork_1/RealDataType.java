e9c55ccaf627c864d9f3a231bee55e7e68d06afd
VoronGit <evilvickers@gmail.com>
2022-07-08 18:14:25 +0300

    Bet updated.

---
+++ src/hillel/homeWork/homeWork_1/RealDataType.java
@@ -0,0 +1,10 @@
+package hillel.homeWork.homeWork_1;
+
+public class RealDataType {
+    public static void main(String[] args) {
+        byte b = 1;         // byte:    размер 8 бит    от -128                         до 127
+        short s = 1;        // short:   размер 16 бит   от -32 768                      до 32 767
+        int i = 1;          // int:     размер 32 бит   от -2 147 483 648               до 2 147 483 647
+        long l = 1;         // long:    размер 64 бит   от -9 223 372 036 854 775 808   до 9 223 372 036 854 775 807
+    }
+}
