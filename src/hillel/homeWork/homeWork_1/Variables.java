0eaa75bc6d8b9d3a7f380b7ab2215c7c06abd487
VoronGit <evilvickers@gmail.com>
2022-07-08 15:13:19 +0300

    HomeWork 1 done.

---
+++ src/hillel/homeWork/homeWork_1/Variables.java
@@ -0,0 +1,19 @@
+package hillel.homeWork.homeWork_1;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 557;
+        short nyShort = 3266;
+        long myLong = 26244286;
+
+        double myDouble = 235.35;
+        float myFloat = 2362.4f;
+
+        char c = 'a';
+        boolean b = true;
+
+        byte myByte = 100;
+
+        System.out.println(myInt);
+    }
+}
