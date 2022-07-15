740267710bcbd414b716e998eb1e1d908d169369
VoronGit <evilvickers@gmail.com>
2022-07-15 16:34:47 +0300

    Storage class added. Bet class updated.

---
+++ src/hillel/homeWork/homeWork_2/Storage.java
@@ -0,0 +1,17 @@
+package hillel.homeWork.homeWork_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Storage {
+    List<Bet> betArray = new ArrayList<>();
+
+    public void addBet(Bet bet) {
+        betArray.add(bet);
+    }
+    public void getBets() {
+        for (Bet bet: betArray) {
+            System.out.println(bet);
+        }
+    }
+}
