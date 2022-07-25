e66471f36ee905a17957926bc7243d4580beb872
VoronGit <evilvickers@gmail.com>
2022-07-25 17:46:09 +0300

    HomeWork 4 done.

--- src/hillel/homeWork/homeWork_4/Phone/Phone.java
+++ src/hillel/homeWork/homeWork_4/Phone/Phone.java
@@ -1,5 +1,39 @@
 package hillel.homeWork.homeWork_4.Phone;
 
-public class Phone {
+public class Phone <T>{
+    T value;
+    static int count;
 
+    public Phone(T value) {
+        this.value = value;
+        count++;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Phone<String> phone = new Phone<>("Nokia");
+        System.out.println(phone.getValue());
+        phone.setValue("Samsung");
+        System.out.println(phone.getValue());
+        System.out.println(Phone.getCount());
+        Phone<String> phone2 = new Phone<>("Apple");
+        System.out.println(phone2.getValue());
+        System.out.println(Phone.getCount());
+        Phone<Integer> phone3 = new Phone<>(3);
+        System.out.println(phone3.getValue());
+        System.out.println(Phone.getCount());
+    }
 }
