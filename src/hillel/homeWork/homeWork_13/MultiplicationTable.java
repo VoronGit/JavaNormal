55b7235227afcd195c6832f9790e95e515ba5da8
VoronGit <evilvickers@gmail.com>
2022-08-24 20:22:31 +0300

    HomeWork 13 is done.

--- src/hillel/homeWork/homeWork_13/MultiplicationTable.java
+++ src/hillel/homeWork/homeWork_13/MultiplicationTable.java
@@ -2,15 +2,14 @@ package hillel.homeWork.homeWork_13;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        for (int i = 2; i <= 9; i++) {
-            for (int j = 2; j <= 5; j++) {
-                System.out.printf("%d * %d = %-2d      ",j,i,i*j);
-            }
-            System.out.println();
-        }
+        getTable(2);
         System.out.println();
+        getTable(6);
+    }
+
+    public static void getTable(int start) {
         for (int i = 2; i <= 9; i++) {
-            for (int j = 6; j <= 9; j++) {
+            for (int j = start; j <= start + 3; j++) {
                 System.out.printf("%d * %d = %-2d      ",j,i,i*j);
             }
             System.out.println();
